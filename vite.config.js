d21cdf8349bb651683d0cb4cd816f5cfdce0678e
jumosd <jumosd@icloud.com>
2023-07-29 00:40:27 +0900

    Chore : 소스코드외 변경사항

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+
+
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
   assetsInclude: ["**/*.mp4"],
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://43.202.4.184:8080",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
