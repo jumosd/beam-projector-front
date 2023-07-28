6ee43d189bfa73b857d9cbf8ef2988a34d80c5fc
jumosd <jumosd@icloud.com>
2023-07-29 04:51:29 +0900

    Feat : 파이어베이스 업로드 셋팅

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
-    apiKey: "AIzaSyAmTx7LZ5KuGGKiJbX4tY7jYpt-RhIwUv0",
-    authDomain: "storage-practise.firebaseapp.com",
-    projectId: "storage-practise",
-    storageBucket: "storage-practise.appspot.com",
-    messagingSenderId: "1066959603977",
-    appId: "1:1066959603977:web:896ed5218e3495a3f7abd8"
+    // ...
+    storageBucket: 'gs://storage-practise.appspot.com'
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+
+// Initialize Cloud Storage and get a reference to the service
+const storage = getStorage(app);
